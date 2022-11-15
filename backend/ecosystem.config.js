a033779bbaa6e1b6cc57ca0fb27f6e635193e8e0
Aleksey <1862647@gmail.com>
2022-11-15 14:02:27 +0700

    fix5

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       path: DEPLOY_PATH,
       'pre-setup': 'rm -rf mesto-backend',
       'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': 'cd ~/mesto-backend/source/backend/ && npm i && npm run build',
+      'post-deploy': 'cd ~/mesto-backend/source/backend/ && ls -la',
     },
   },
 };
