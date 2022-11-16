d93808043952f0f6d270eeb974fea36ed68e0c1f
Aleksey <1862647@gmail.com>
2022-11-16 10:46:48 +0700

    change proj dir

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-setup': 'rm -rf mesto-backend',
+      'pre-setup': 'rm -rf mesto',
       'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy': 'cd ~/mesto/source/backend/ && npm i && npm run build && npx pm2 start',
     },
