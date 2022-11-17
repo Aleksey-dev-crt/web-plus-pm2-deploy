89443ee98b161371757a875f2e7caf44d7e3edae
Aleksey <1862647@gmail.com>
2022-11-17 16:35:21 +0700

    fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 
 const getResponse = (res) => {
-  // console.log('res', res.json().then(data => console.log('data', data)))
-    return res.ok ? res.json() : Promise.reject(res.json().then(err => err.data));
+  console.log('res', res.json().then(data => data))
+    return res.ok ? res.json() : Promise.reject({status: res.status, message: res});
 }
 
 class Api {
